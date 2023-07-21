0ad1015a5c8ea417724a9507608401116f341eeb
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2023-07-21 17:41:19 -0400

    added DenoBlogSite project, and made projects page mobile responsive.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -14,8 +14,12 @@ const Projects = () => {
         <h2>My Projects</h2>
         <h3>More Projects Coming Soon!</h3>
         <div className='project-container'>
-            <iframe className='task-frame' width={360} height={580} src='https://task-manager-carlogy.vercel.app/'></iframe>
+
+            <iframe className='project-frame' width={360} height={600} src='https://task-manager-carlogy.vercel.app/'></iframe>
+
+            <iframe className='project-frame' width={360} height={600} src='https://carlogyblog.deno.dev/'></iframe>
         </div>
+
         </div>
         <div>
           <Link to="/" className="btn">Back</Link>
